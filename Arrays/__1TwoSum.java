9037b4181f8efabeca092c4832cad7238df3e8e6
yuvrajdas <yuvrajdas1531@gmail.com>
2023-08-28 21:21:53 +0530

    tow sum

--- Arrays/__1TwoSum.java
+++ Arrays/__1TwoSum.java
@@ -1,6 +1,25 @@
 package Arrays;
+import java.util.*;
 public class __1TwoSum {
+
+    public static int[] towSum(int arr[], int target){
+        int res[] = {-1,-1};
+        HashMap<Integer, Integer> map = new HashMap<>();
+        
+        for(int i=0; i<arr.length; i++){
+            if(map.containsKey(target-arr[i])){
+                res[0] = i;
+                res[1] = map.get(target-arr[i]);
+            }
+
+            map.put(arr[i], i);
+        }
+        return res;
+    }
     public static void main(String[] args) {
-        System.out.println("Hello");
+        int arr[] = {2,7,11,15};
+        int target = 9;
+        int res[] = towSum(arr, target);
+        System.out.println(Arrays.toString(res));
     }
 }
